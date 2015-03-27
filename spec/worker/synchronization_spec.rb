43314c40f78bb8829efc640cb2da136da8538e00
Jared Jenkins <jaredj@squareup.com>
2015-03-27 12:27:47 -0700

    Fix rubocop issues

--- spec/worker/synchronization_spec.rb
+++ spec/worker/synchronization_spec.rb
@@ -1,27 +1,27 @@
-require 'spec_helper'
+require "spec_helper"
 
 RSpec.describe Sqeduler::Worker::Synchronization do
-  describe '.synchronize' do
+  describe ".synchronize" do
     before do
       stub_const(
-        'ParentWorker',
+        "ParentWorker",
         Class.new do
           prepend Sqeduler::Worker::Synchronization
-          synchronize :one_at_a_time, expiration: 10, timeout: 1
+          synchronize :one_at_a_time, :expiration => 10, :timeout => 1
         end
       )
 
-      stub_const('ChildWorker', Class.new(ParentWorker))
+      stub_const("ChildWorker", Class.new(ParentWorker))
     end
 
-    it 'should preserve the synchronize attributes' do
+    it "should preserve the synchronize attributes" do
       expect(ChildWorker.synchronize_jobs_mode).to eq(:one_at_a_time)
       expect(ChildWorker.synchronize_jobs_expiration).to eq(10)
       expect(ChildWorker.synchronize_jobs_timeout).to eq(1)
     end
 
-    it 'should allow the child class to update the synchronize attributes' do
-      ChildWorker.synchronize :one_at_a_time, expiration: 20, timeout: 2
+    it "should allow the child class to update the synchronize attributes" do
+      ChildWorker.synchronize :one_at_a_time, :expiration => 20, :timeout => 2
       expect(ChildWorker.synchronize_jobs_mode).to eq(:one_at_a_time)
       expect(ChildWorker.synchronize_jobs_expiration).to eq(20)
       expect(ChildWorker.synchronize_jobs_timeout).to eq(2)
