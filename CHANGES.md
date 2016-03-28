e46712d279ff755df153b8a7699d8b95f441d5c0
Gabriel Gilder <gabriel@squareup.com>
2016-03-28 16:14:34 -0700

    Add ability to use provided ConnectionPool

--- CHANGES.md
+++ CHANGES.md
@@ -1,3 +1,7 @@
+### 0.3.4 / 2016-03-28
+
+* Add ability to use a client-provided connection pool rather than creating one
+
 ### 0.3.3 / 2016-03-25
 
 * Fixed lock refresher not calling `redis_pool` properly so it wouldn't actually run
@@ -5,7 +9,7 @@
 ### 0.3.2 / 2016-03-10
 
 * Fixed lock refresher failing to lock properly for exclusive runs
-* Added debug logs for lock refresher 
+* Added debug logs for lock refresher
 
 ### 0.3.1 / 2016-02-17
 
--- lib/sqeduler/config.rb
+++ lib/sqeduler/config.rb
@@ -2,11 +2,12 @@
 module Sqeduler
   # Simple config for Sqeduler::Service
   class Config
-    attr_accessor :logger, :redis_hash, :schedule_path,
+    attr_accessor :logger, :redis_hash, :redis_pool, :schedule_path,
                   :on_server_start, :on_client_start, :maintain_locks
 
     def initialize(opts = {})
       self.redis_hash = opts[:redis_hash]
+      self.redis_pool = opts[:redis_pool]
       self.schedule_path = opts[:schedule_path]
       self.on_server_start = opts[:on_server_start]
       self.on_client_start = opts[:on_client_start]
